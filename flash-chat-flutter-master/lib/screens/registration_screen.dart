83ecc029e5d5c049ae608ab94da23c12bd327ef0
Ralph Lee <ralph.lee@live.ca>
2023-08-09 20:20:46 +0900

    Add: Create a new user with Firebase Auth

--- flash-chat-flutter-master/lib/screens/registration_screen.dart
+++ flash-chat-flutter-master/lib/screens/registration_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flash_chat/constants.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/components/round_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = '/registration_screen';
@@ -10,6 +12,7 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _auth = FirebaseAuth.instance;
   late String email;
   late String password;
 
@@ -58,9 +61,19 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             RoundButton(
               title: 'Register',
               colour: Colors.blueAccent,
-              onPressed: () {
-                print(email);
-                print(password);
+              onPressed: () async {
+                // print(email);
+                // print(password);
+
+                try {
+                  final newUser = await _auth.createUserWithEmailAndPassword(
+                      email: email, password: password);
+                  if (newUser != null) {
+                    Navigator.pushNamed(context, ChatScreen.id);
+                  }
+                } catch (e) {
+                  print(e);
+                }
               },
             ),
           ],
