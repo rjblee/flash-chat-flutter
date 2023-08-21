353054a44b8dacefda806b4aae5c764f6e194960
Ralph Lee <ralph.lee@live.ca>
2023-08-21 22:38:03 +0900

    Add: modal_progress_hud depdendency

--- flash-chat-flutter-master/lib/screens/chat_screen.dart
+++ flash-chat-flutter-master/lib/screens/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class ChatScreen extends StatefulWidget {
   static const String id = '/chat_screen';
