525d16e4da001987d3711bf05dbed84c757a3b23
Ralph Lee <ralph.lee@live.ca>
2023-08-26 23:56:56 +0900

    Add: Create a stream of messages to listen to from Firebase

--- flash-chat-flutter-master/lib/screens/chat_screen.dart
+++ flash-chat-flutter-master/lib/screens/chat_screen.dart
@@ -36,6 +36,17 @@ class _ChatScreenState extends State<ChatScreen> {
 
   void getMessages() async {
     final messages = await _firestore.collection('messages').get();
+    for (var message in messages.docs) {
+      print(message.data());
+    }
+  }
+
+  void messagesStream() async {
+    await for (var snapshot in _firestore.collection('messages').snapshots()) {
+      for (var message in snapshot.docs) {
+        print(message.data());
+      }
+    }
   }
 
   @override
@@ -47,7 +58,8 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: Icon(Icons.close),
               onPressed: () {
-                _auth.signOut().then((value) => Navigator.pop(context));
+                messagesStream();
+                // _auth.signOut().then((value) => Navigator.pop(context));
               }),
         ],
         title: Text('⚡️Chat'),
